45b36c46cbd752815150819e519ac542af82dd4d
AkshajP3 <aputtoju316@gmail.com>
2025-07-14 22:15:36 -0400

    Added Comments

--- gradebook.js
+++ gradebook.js
@@ -1,9 +1,11 @@
 function fetchGradeData() {
     console.log("Fetching grade data...")
+    //Gets data from database
 }
 
 function populateGradebook(data){
     console.log("Populating gradebook with data:", data);
+    //Puts data into gradebook 
 }
 
 const gradeData = fetchGradeData()
